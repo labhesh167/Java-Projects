6f5310ed96c727669ab9b2a05bd5392335db6f44
Labhesh Chaudhari <78224580+labhesh167@users.noreply.github.com>
2023-10-08 01:01:59 +0530

    Update LaunchGame.java
    ramdom method immplemented

--- Guessergame/LaunchGame.java
+++ Guessergame/LaunchGame.java
@@ -1,106 +1,82 @@
-
 import java.util.Scanner;
 
-class Player
-{
+class Player {
     int numGuess;
-    public void guessNumber()
-    {
+
+    public void guessNumber(int i) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("player please Guess the number");
+        System.out.println("player " + i + " please Guess the number");
         numGuess = sc.nextInt();
     }
 
 }
-class Guesser
-{
+
+class Guesser {
     int numGuess;
-    public void guessNumber() 
-    {
+
+    public void guessNumber() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Guess the number");
-        numGuess = sc.nextInt();
+        numGuess = (int) (Math.random() * 10);
+        System.out.println(numGuess);
     }
-    
+
 }
-class Umpire
-{
+
+class Umpire {
     static Guesser guesser;
-    Player p1,p2,p3;
-    public static void getNumberFromGuesser()
-    {
-    guesser = new Guesser();
-    guesser.guessNumber();
+    Player p1, p2, p3;
+
+    public void getNumberFromGuesser() {
+        guesser = new Guesser();
+        guesser.guessNumber();
 
     }
 
-    public void collectFromPlayer() 
-    {
+    public void collectFromPlayer() {
         p1 = new Player();
         p2 = new Player();
         p3 = new Player();
-        p1.guessNumber();
-        p2.guessNumber();
-        p3.guessNumber();
-        
+        p1.guessNumber(1);
+        p2.guessNumber(2);
+        p3.guessNumber(3);
+
     }
 
-    public void calcResult() 
-    {
+    public void calcResult() {
         System.out.println(guesser.numGuess + " " + p1.numGuess + " " + p2.numGuess + " " + p3.numGuess);
-        if(guesser.numGuess == p1.numGuess)
-        {
-            if(guesser.numGuess == p2.numGuess && guesser.numGuess == p3.numGuess)
-            {
+        if (guesser.numGuess == p1.numGuess) {
+            if (guesser.numGuess == p2.numGuess && guesser.numGuess == p3.numGuess) {
                 System.out.println("All are winners");
-            }
-            else if(guesser.numGuess == p2.numGuess)
-            {
+            } else if (guesser.numGuess == p2.numGuess) {
                 System.out.println("Player 1 and player 2 are winners");
-            }
-            else if(guesser.numGuess == p3.numGuess)
-            {
+            } else if (guesser.numGuess == p3.numGuess) {
                 System.out.println("Player 1 and player 3 are winners");
-            }
-            else
-            {
+            } else {
                 System.out.println("Player 1 is the winner");
             }
-        }
-        else if(guesser.numGuess == p2.numGuess)
-        {
-            if(guesser.numGuess == p3.numGuess)
-            {
-             System.out.println("Player 2 and Player 3 are winners");
-            }
-            else
-            {
+        } else if (guesser.numGuess == p2.numGuess) {
+            if (guesser.numGuess == p3.numGuess) {
+                System.out.println("Player 2 and Player 3 are winners");
+            } else {
                 System.out.println("Player 2 is the winner");
             }
-        }
-        else if(guesser.numGuess == p3.numGuess)
-        {
+        } else if (guesser.numGuess == p3.numGuess) {
             System.out.println("Player 3 is winner");
-        }
-        else
-        {
+        } else {
             System.out.println("Better luch next time");
-        } 
+        }
 
     }
 
 }
 
-
-public class LaunchGame 
-{
-    public static void main(String[] args)
-    {
-
+public class LaunchGame {
+    public static void main(String[] args) {
         Umpire umpire = new Umpire();
         umpire.getNumberFromGuesser();
         umpire.collectFromPlayer();
         umpire.calcResult();
-    }    
+    }
 
 }
